7945f18d7bea98262a0ba205d72cded4fa0bbae2
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-03 17:15:02 -0500

    Updated to have a method handler rather than a type handler because that's where my annotation goes.

--- src/main/java/io/crdant/spring/alexa/web/IntentRequestMappingHandlerMapping.java
+++ src/main/java/io/crdant/spring/alexa/web/IntentRequestMappingHandlerMapping.java
@@ -1,9 +1,11 @@
 package io.crdant.spring.alexa.web;
 
-import io.crdant.spring.alexa.web.annotation.IntentController;
-import io.crdant.spring.alexa.web.annotation.IntentMapping;
 import io.crdant.spring.alexa.condition.IntentRequestCondition;
+import io.crdant.spring.alexa.web.annotation.IntentMapping;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.condition.RequestCondition;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
@@ -11,15 +13,18 @@ import java.lang.reflect.Method;
 
 public class IntentRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
 
-    @Override protected RequestCondition<?> getCustomTypeCondition(Class<?> handlerType) {
-        IntentController typeAnnotation = AnnotationUtils.findAnnotation(handlerType, IntentController.class);
-        logger.debug("Searched for class annotation @IntentContoller on " + handlerType.getName() + " and found " + typeAnnotation);
-        return (typeAnnotation != null) ? new IntentRequestCondition() : null;
+    @Override
+    protected boolean isHandler(Class<?> beanType) {
+        return (AnnotatedElementUtils.hasAnnotation(beanType, Controller.class) ||
+                AnnotatedElementUtils.hasAnnotation(beanType, RequestMapping.class) ||
+                AnnotatedElementUtils.hasAnnotation(beanType, IntentMapping.class));
     }
 
-    @Override protected RequestCondition<?> getCustomMethodCondition(Method handlerMethod) {
+    @Override
+    protected RequestCondition<?> getCustomMethodCondition(Method handlerMethod) {
         IntentMapping methodAnnotation = AnnotationUtils.findAnnotation(handlerMethod, IntentMapping.class);
         logger.debug("Searched for method annotation @IntentMapping on " + handlerMethod.getName() + " and found " + methodAnnotation);
-        return (methodAnnotation != null) ? new IntentRequestCondition( methodAnnotation.value() ) : null;
+        return (methodAnnotation != null) ? new IntentRequestCondition(methodAnnotation.value()) : super.getCustomMethodCondition(handlerMethod);
     }
+
 }
