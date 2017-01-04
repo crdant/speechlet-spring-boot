90c293c1595e4ff5d5c3bf0212201f66aebc843d
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-04 16:02:39 -0500

    Added an Interceptor that performs Amazon's required checks.

---
+++ src/main/java/io/crdant/spring/alexa/speechlet/handler/SpeechletValidationHandlerInterceptor.java
@@ -0,0 +1,43 @@
+package io.crdant.spring.alexa.speechlet.handler;
+
+import com.amazon.speech.Sdk;
+import com.amazon.speech.speechlet.authentication.SpeechletRequestSignatureVerifier;
+import com.amazon.speech.speechlet.verifier.TimestampSpeechletRequestVerifier;
+import io.crdant.spring.alexa.speechlet.web.SpeechletServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
+
+public class SpeechletValidationHandlerInterceptor extends HandlerInterceptorAdapter {
+
+    @Value("speechlet.validation.disable")
+    boolean disable ;
+
+    @Value("speechlet.timestamp.tolerance")
+    long tolerance ;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ( !(request instanceof SpeechletServletRequest ) ) return false;
+        SpeechletServletRequest speechletServletRequest = (SpeechletServletRequest) request ;
+        return disable || ( validSignature(speechletServletRequest) && validTimetamp(speechletServletRequest) );
+    }
+
+    private boolean validSignature(SpeechletServletRequest request) {
+        try {
+            SpeechletRequestSignatureVerifier.checkRequestSignature(request.getSerializedRequest(), request.getHeader(Sdk.SIGNATURE_REQUEST_HEADER),
+                    request.getHeader(Sdk.SIGNATURE_CERTIFICATE_CHAIN_URL_REQUEST_HEADER));
+            return true ;
+        } catch ( SecurityException invalidSignature ) {
+            return false ;
+        }
+    }
+
+    private boolean validTimetamp(SpeechletServletRequest request) {
+        TimestampSpeechletRequestVerifier verifier = new TimestampSpeechletRequestVerifier(tolerance, TimeUnit.SECONDS);
+        return verifier.verify(request.getSpeechletRequest(), request.getSpeechletSession());
+    }
+}
