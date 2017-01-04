950d0b9ee43aa4fba0f84d76f4f21cd9c8565a25
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-03 22:15:20 -0500

    Optimized imports.

--- src/main/java/io/crdant/spring/alexa/beans/AlexaSkillRegistration.java
+++ src/main/java/io/crdant/spring/alexa/beans/AlexaSkillRegistration.java
@@ -2,9 +2,11 @@ package io.crdant.spring.alexa.beans;
 
 import io.crdant.spring.alexa.web.IntentRequestMappingHandlerMapping;
 import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 @Configuration
 public class AlexaSkillRegistration extends WebMvcRegistrationsAdapter {
