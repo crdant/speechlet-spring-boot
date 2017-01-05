0cc229b60f70e62d807cff0ed64bfc433138f916
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-05 17:10:07 -0500

    Renamed class without Alexa in it and changed mean names to be qualified with package names.

--- src/main/java/io/crdant/spring/alexa/beans/SpeechletConfiguration.java
+++ src/main/java/io/crdant/spring/alexa/beans/SpeechletConfiguration.java
@@ -1,24 +1,44 @@
 package io.crdant.spring.alexa.beans;
 
 import io.crdant.spring.alexa.annotation.SlotArgumentResolver;
+import io.crdant.spring.alexa.speechlet.handler.SpeechletHandlerMapping;
+import io.crdant.spring.alexa.speechlet.handler.SpeechletValidationHandlerInterceptor;
 import io.crdant.spring.alexa.speechlet.method.SpeechletMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 @Configuration
-public class AlexaSkillConfiguration extends WebMvcConfigurerAdapter  {
+public class SpeechletConfiguration extends WebMvcConfigurerAdapter  {
+
+    @Autowired
+    SpeechletValidationHandlerInterceptor speechletValidationHandlerInterceptor ;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new SlotArgumentResolver());
     }
 
-    @Bean
+    @Bean("io.crdant.spring.alexa.speechlet.handler.SpeechletHandlerMapping")
+    public HandlerMapping speechletHandlerMapping () {
+        return new SpeechletHandlerMapping();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(speechletValidationHandlerInterceptor);
+        super.addInterceptors(registry);
+    }
+
+    @Bean("io.crdant.spring.alexa.speechlet.method.SpeechletHandlerMapping")
     public HttpMessageConverter speechletMessageConverter() {
         return new SpeechletMessageConverter();
     }
