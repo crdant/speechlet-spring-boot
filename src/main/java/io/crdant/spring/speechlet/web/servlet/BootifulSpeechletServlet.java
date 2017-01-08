35bf0c74d4f5698ec6256acd0ac440d2754db535
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-07 19:02:38 -0500

    Simplified delegation to handler since it wasn't really adding value.

--- src/main/java/io/crdant/spring/speechlet/web/servlet/BootifulSpeechletServlet.java
+++ src/main/java/io/crdant/spring/speechlet/web/servlet/BootifulSpeechletServlet.java
@@ -1,45 +1,54 @@
 package io.crdant.spring.speechlet.web.servlet;
 
-import com.amazon.speech.Sdk;
-import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.json.SpeechletResponseEnvelope;
+import com.amazon.speech.speechlet.SpeechletRequestDispatcher;
 import com.amazon.speech.speechlet.SpeechletRequestHandler;
 import com.amazon.speech.speechlet.SpeechletRequestHandlerException;
 import com.amazon.speech.speechlet.SpeechletV2;
-import com.amazon.speech.speechlet.authentication.SpeechletRequestSignatureVerifier;
-import com.amazon.speech.speechlet.servlet.SpeechletServlet;
+import io.crdant.spring.speechlet.SpeechletMapping;
 import io.crdant.spring.speechlet.web.SpeechletServletRequest;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
 @Component
-public class BootifulSpeechletServlet extends SpeechletServlet {
+public class BootifulSpeechletServlet extends HttpServlet {
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    SpeechletRequestHandler speechletRequestHandler;
+    SpeechletMapping mapping ;
 
     protected SpeechletV2 speechlet ;
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        SpeechletServletRequest speechletServletRequest = new SpeechletServletRequest(request);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        SpeechletServletRequest speechletServletRequest = null ;
+
+        try {
+            speechletServletRequest = new SpeechletServletRequest(request);
+        } catch ( IllegalArgumentException ex ) {
+            int statusCode = HttpServletResponse.SC_BAD_REQUEST;
+            response.sendError(statusCode, ex.getMessage());
+            return;
+        }
+
+        SpeechletV2 speechlet = lookupSpeechlet(speechletServletRequest);
 
         byte[] outputBytes = null ;
         try {
-            outputBytes =
-                    speechletRequestHandler.handleSpeechletCall(speechlet,
-                            speechletServletRequest.getSerializedRequest());
+            // Dispatch request to Speechlet
+            SpeechletRequestDispatcher dispatcher = new SpeechletRequestDispatcher(speechlet);
+            SpeechletResponseEnvelope responseEnvelope =
+                    dispatcher.dispatchSpeechletCall(speechletServletRequest.getRequestEnvelope(), speechletServletRequest.getSpeechletSession());
+            outputBytes = responseEnvelope.toJsonBytes();
         } catch (SpeechletRequestHandlerException | SecurityException ex) {
             int statusCode = HttpServletResponse.SC_BAD_REQUEST;
             logger.error("Exception occurred in doPost, returning status code {}", statusCode, ex);
@@ -59,6 +68,20 @@ public class BootifulSpeechletServlet extends SpeechletServlet {
             response.setContentLength(outputBytes.length);
             out.write(outputBytes);
         }
+        logger.debug("About to call the superclass, you should have seen some output by now");
         super.doPost(request, response);
     }
+
+    /**
+     * Load a Speechlet for the given applicationId. Speechlets that are annoted with the
+     * Speechlet annotation will be located and tested to see if they are mapped to the same
+     * given application.
+     *
+     * @param request
+     * @return
+     */
+    protected SpeechletV2 lookupSpeechlet (SpeechletServletRequest request ) {
+        return mapping.lookupSpeechlet(request.getApplicationId());
+    }
+
 }
