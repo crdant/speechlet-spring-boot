410b0dcfd879a6d1ee20e31b61bf7960da961067
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-08 00:28:03 -0500

    Implemented applicaiton id validation using annotations on speechlet beans.

--- src/main/java/io/crdant/spring/speechlet/web/filter/SpeechletValidationServletFilter.java
+++ src/main/java/io/crdant/spring/speechlet/web/filter/SpeechletValidationServletFilter.java
@@ -3,10 +3,12 @@ package io.crdant.spring.speechlet.web.filter;
 import com.amazon.speech.Sdk;
 import com.amazon.speech.speechlet.authentication.SpeechletRequestSignatureVerifier;
 import com.amazon.speech.speechlet.verifier.TimestampSpeechletRequestVerifier;
+import io.crdant.spring.speechlet.annotation.Speechlet;
 import io.crdant.spring.speechlet.web.SpeechletServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,10 +17,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public class SpeechletValidationServletFilter extends OncePerRequestFilter {
+public class SpeechletValidationServletFilter extends OncePerRequestFilter  {
 
     @Value("${speechlet.validation.disable}")
     Boolean disable ;
@@ -28,6 +33,8 @@ public class SpeechletValidationServletFilter extends OncePerRequestFilter {
 
     @Autowired
     ApplicationContext applicationContext ;
+
+    private Set<String> applicationIds ;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -46,6 +53,18 @@ public class SpeechletValidationServletFilter extends OncePerRequestFilter {
         } else {
             response.sendError( HttpServletResponse.SC_FORBIDDEN, "Request violates security pre-conditions for an Alexa skill" ) ;
         }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws ServletException {
+        this.applicationIds = new HashSet<String>();
+        Map<String,Object> speechlets = applicationContext.getBeansWithAnnotation(Speechlet.class);
+        for ( Map.Entry<String,Object> speechlet : speechlets.entrySet() ) {
+            Class<?> bean = speechlet.getValue().getClass();
+            Speechlet annotation = AnnotationUtils.findAnnotation(bean, Speechlet.class);
+            applicationIds.add(annotation.value()[0]);
+        }
+        super.afterPropertiesSet();
     }
 
     private boolean validSignature(SpeechletServletRequest request) {
@@ -70,8 +89,7 @@ public class SpeechletValidationServletFilter extends OncePerRequestFilter {
     }
 
     private boolean validApplication(SpeechletServletRequest request) {
-        logger.debug("will test the application ids here someday...");
-        return true ;
+        return applicationIds.contains(request.getApplicationId());
     }
 
 }
