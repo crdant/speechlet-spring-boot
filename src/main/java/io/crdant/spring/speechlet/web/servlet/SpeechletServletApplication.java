15dfa86cbe202bffc058c0647534ff49469fa66a
Chuck D'Antonio <c_dantonio@acm.org>
2017-01-07 19:00:29 -0500

    Moved bean creation into the application class.

--- src/main/java/io/crdant/spring/speechlet/web/servlet/SpeechletServletApplication.java
+++ src/main/java/io/crdant/spring/speechlet/web/servlet/SpeechletServletApplication.java
@@ -1,21 +1,30 @@
 package io.crdant.spring.speechlet.web.servlet;
 
+import io.crdant.spring.speechlet.web.filter.SpeechletRequestServletFilter;
+import io.crdant.spring.speechlet.web.filter.SpeechletValidationServletFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.Ordered;
 
 import javax.servlet.Servlet;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
+@ComponentScan("io.crdant.spring.speechlet")
 public class SpeechletServletApplication extends SpringBootServletInitializer {
 
     @Autowired
     Servlet speechletServlet ;
+
+    @Autowired
+    SpeechletValidationServletFilter speechletValidationFilter;
 
     @SuppressWarnings("serial")
     @Bean
@@ -26,6 +35,14 @@ public class SpeechletServletApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpeechletServletApplication.class);
+    }
+
+    @Bean
+    public FilterRegistrationBean speechletValidationFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(speechletValidationFilter);
+        registration.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return registration;
     }
 
     public static void main(String[] args) throws Exception {
